0562f255f4df123ba3a4e17af9403c0763b4c436
MisterShika <125979751+MisterShika@users.noreply.github.com>
2025-11-14 14:56:23 +0900

    Small minor design change to modals.

--- components/ConnectReport.js
+++ components/ConnectReport.js
@@ -8,17 +8,19 @@ export default function ConnectReport({ onClose }) {
       onClick={onClose} // handle background click
     >
       <div
-        className="bg-white p-6 rounded-lg shadow-lg text-center max-w-md w-full"
+        className="bg-white pt-0 pb-6 px-2 rounded-lg shadow-lg text-center max-w-md w-full"
         onClick={(e) => e.stopPropagation()} // prevent modal content clicks from closing
       >
+        <div className="flex flex-row-reverse">
+          <button
+            className="mt-2 p-2 bg-gray-300 rounded hover:bg-gray-400"
+            onClick={onClose} // also close with button
+          >
+          ✖
+          </button>
+        </div>
         <h2 className="text-xl font-bold mb-4">レポートを追加</h2>
         <p>モーダルが表示されています。</p>
-        <button
-          className="mt-4 px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
-          onClick={onClose} // also close with button
-        >
-          閉じる
-        </button>
       </div>
     </div>
   );
